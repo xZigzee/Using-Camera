d7d2477484921038a932c0877f74abefdce0f60e
xZigzee <callum.w@hotmail.co.uk>
2016-02-02 14:31:08 +0000

    ttt

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function onDeviceReady() {
 }
 
 function capturePhoto() {
-	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 100,
 	destinationType: destinationType.DATA_URL });
 }
 	
