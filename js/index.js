b08b302929be8ec6fc9e101dc7166f4d491c16e1
xZigzee <callum.w@hotmail.co.uk>
2016-02-02 14:08:45 +0000

    LOL:)

--- js/index.js
+++ js/index.js
@@ -1,37 +1,23 @@
-$(document).on("pagecreate","#pageone", onPageCreated);
 
-//when page is loaded
-function onPageCreated() {
-	
-	//setup listern for change in image input element
-	var input = document.querySelector('input[type=file]');
-	input.onchange = function () {
-  		var file = input.files[0];
-		displayAsImage(file);  //call this fucntion with the file name if the image once taken.
-	};
+var destinationType; //used sets what should be returned (image date OR file path to image for example)
+
+document.addEventListener("deviceready",onDeviceReady,false);
+
+function onDeviceReady() {
+	destinationType=navigator.camera.DestinationType;
 }
 
-
-
-function displayAsImage(file) {
+function capturePhoto() {
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+	destinationType: destinationType.DATA_URL });
+}
 	
-	//create a HTML image
-  	var imgURL = URL.createObjectURL(file);
-  	var img = document.createElement('img');
-
-	//when the image is loading get the URL location of the file.
-  	img.onload = function() {
-    	URL.revokeObjectURL(imgURL);
-  	};
-
-	//set the img URL
-  	img.src = imgURL;
-  
-  	//fix the size of the image
-  	img.width = 200;
-  	img.height = 200;
-  
-  	//insert the image intothe DOM so its displayed.
-  	$('#imagePreview').html(img);
+function onPhotoDataSuccess(imageData) {
+	var image = document.getElementById('image');
+	image.style.display = 'block';
+	image.src = "data:image/jpeg;base64," + imageData;
 }
 
+function onFail(message) {
+      alert('Failed because: ' + message);
+}
